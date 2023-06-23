fc28b577d59eb753ceb2053b16e3fab31d615df8
MisterPoney <marcosandreo2003@gmail.com>
2023-06-23 11:10:51 -0300

    backend

---
+++ Back/ProjectStag2/src/main/java/andreo/marcos/ProjectStag2/ProjectStag2Application.java
@@ -0,0 +1,13 @@
+package andreo.marcos.ProjectStag2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStag2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStag2Application.class, args);
+	}
+
+}
---
+++ Back/ProjectStag2/src/test/java/andreo/marcos/ProjectStag2/ProjectStag2ApplicationTests.java
@@ -0,0 +1,13 @@
+package andreo.marcos.ProjectStag2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStag2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
